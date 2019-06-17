2837bfed0d90a81ead342388d14229ed15440ef3
rob <robert.szczepanski@cmsrs.pl>
2019-06-17 18:07:56 +0200

    frontend follow-up - blade fix

--- resources/views/cms.blade.php
+++ resources/views/cms.blade.php
@@ -1,21 +1,25 @@
 @extends('layouts.default')
 @section('content')
+
+  <h1 class="mb-4 mt-3">{{$page->title}}</h1>
+
   @if ( $page->type  === 'cms')
 
-      @if ( $page->images)
-        <?php foreach ($page->images as $image) {?>
-          <img class="m-2" src="{{$image->getHtmlImage()}}" alt="{{$image->name}}" />
-        <?php }?>
+      @if( $page->images)
+        @foreach($page->images as $image)
+            <img class="m-3" src="{{$image->getHtmlImage()}}" alt="{{$image->name}}" />
+        @endforeach
       @endif
+
       <div class="mt-2">{{$page->content}}</div>
 
-  @elseif ($page->type  === 'gallery')
+  @elseif($page->type  === 'gallery')
 
-    @if ( $page->images)
-      <?php foreach ($page->images as $image) {?>
-        <img class="m-2" src="<?php echo  $image->getHtmlImage(); ?>" />
-      <?php }?>
-    @endif
+      @if( $page->images)
+        @foreach($page->images as $image)
+            <img class="m-3" src="{{$image->getHtmlImage()}}" alt="{{$image->name}}" />
+        @endforeach
+      @endif
 
   @endif
 @stop
