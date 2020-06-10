69f9b6c6bd52bf4ea3a306ff9fd5cdc0d95b210d
Robert Szczepanski <robert.szczepanski@cmsrs.pl>
2020-06-10 18:01:54 +0200

    fix for laravel7

--- config/app.php
+++ config/app.php
@@ -161,7 +161,8 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-        Tymon\JWTAuth\Providers\LaravelServiceProvider::class,
+        //Tymon\JWTAuth\Providers\LaravelServiceProvider::class,
+        //Tymon\JWTAuth\Providers\JWTAuthServiceProvider::class,
         Intervention\Image\ImageServiceProvider::class,
 
         /*
