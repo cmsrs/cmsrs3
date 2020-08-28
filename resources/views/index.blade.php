196a3815fdd5eb396039505821732b6dd0cbeede
Robert Szczepanski <robert.szczepanski@cmsrs.pl>
2020-08-28 15:49:28 +0200

    fix demo functionality

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,11 +1,18 @@
 @extends('layouts.default')
 @section('content')
 <div class="starter-template">
-  <h1>Demo version</h1>
+
+  @if( env('DEMO_STATUS') )
+    <h1>Demo version</h1>
+  @else
+    <h1>cmsRS</h1>
+  @endif
   <div>
-    <p class="lead">The demo version was created for demonstration purposes.<p>
-    <div class="alert alert-danger" role="alert">Saving, updating, deleting a single record has been disabled.</div>
-    <br><br>
+    @if( env('DEMO_STATUS') )
+      <p class="lead">The demo version was created for demonstration purposes.<p>
+      <div class="alert alert-danger" role="alert">Saving, updating, deleting a single record has been disabled.</div>
+      <br><br>
+    @endif    
     <p class="lead">    
     Login to the admin panel: <a href="{{ config('app.url') }}/admin">{{ config('app.url') }}/admin</a>
     <!--    
