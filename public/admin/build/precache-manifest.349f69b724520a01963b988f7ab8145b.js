8fe8ab25a23cf1e3b13ed36e14d721f3e10f284e
Robert Szczepanski <robert.szczepanski@cmsrs.pl>
2020-09-07 13:09:50 +0200

    admin - fix

---
+++ public/admin/build/precache-manifest.349f69b724520a01963b988f7ab8145b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21f0243c887b2570f373956423d3ce65",
+    "url": "/admin/index.html"
+  },
+  {
+    "revision": "9669fd4138698a474754",
+    "url": "/admin/static/css/2.256b788b.chunk.css"
+  },
+  {
+    "revision": "9669fd4138698a474754",
+    "url": "/admin/static/js/2.569eb30c.chunk.js"
+  },
+  {
+    "revision": "ab6d377e8634e726a5e3",
+    "url": "/admin/static/js/main.50b7f3b8.chunk.js"
+  },
+  {
+    "revision": "7c4f45329e47798217a3",
+    "url": "/admin/static/js/runtime~main.b3695f6e.js"
+  }
+]);
--- public/admin/precache-manifest.a65ec4d26119721ebcf2a372e16c10de.js
+++ public/admin/precache-manifest.a65ec4d26119721ebcf2a372e16c10de.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "1906ca72e19e1a9ad8c4f695cf581cea",
+    "revision": "d36a141863e2288595c3ed6354ac95c6",
     "url": "/admin/index.html"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/admin/static/js/2.cfbb0243.chunk.js"
   },
   {
-    "revision": "b69df567adfb5a654a32",
-    "url": "/admin/static/js/main.e642e128.chunk.js"
+    "revision": "ae8eefffc122e0c086e9",
+    "url": "/admin/static/js/main.6a29c85b.chunk.js"
   },
   {
     "revision": "7c4f45329e47798217a3",
