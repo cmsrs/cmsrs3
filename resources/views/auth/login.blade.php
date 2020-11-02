38fee0483fdd29e1a09f37c9069b5db0f2b92246
Robert Szczepanski <robert.szczepanski@cmsrs.pl>
2020-11-02 17:05:16 +0100

    translate into polish

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,7 +2,7 @@
 
 
 @section('content')
-<div class="container">
+<div class="container mt-5">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
