74cb6366467d89c740d22afc8ec95317527cbab6
cmsrs <robert.szczepanski@cmsrs.pl>
2020-11-11 19:33:39 +0100

    fix in server

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,10 +1,8 @@
-@extends('layouts.default')
+@extends('layouts.index')
 @section('content')
-<div class="starter-template">
 
 @if( $page->translatesByColumnAndLang( 'content', $lang ) )
   {!! $page->translatesByColumnAndLang( 'content', $lang ) !!}
 @endif
 
-</div>
 @stop
---
+++ resources/views/layouts/index.blade.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+<head>
+   @include('includes.head')
+</head>
+<body>
+@include('includes.header')
+
+
+<main role="main">
+@yield('content')
+</main><!-- /.container -->
+
+
+@include('includes.footer')
+</body>
+</html>
