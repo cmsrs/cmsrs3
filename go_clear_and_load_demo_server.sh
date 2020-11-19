2a894b6a25049dfe3e3f5af060e54b9b6f92ebd6
cmsrs <robert.szczepanski@cmsrs.pl>
2020-11-19 13:13:34 +0100

    demo data update

--- go_clear_and_load_demo_server.sh
+++ go_clear_and_load_demo_server.sh
@@ -2,6 +2,9 @@ rm -rf public/images/page
 rm -rf public/images/product
 #./go_privilege.sh 
 
-php74 artisan migrate:refresh
+php74 artisan migrate:fresh
 php74 artisan db:seed
 php74 artisan command:load-demo-data
+
+php74 artisan  cache:clear
+php74  artisan command:create-site-map
