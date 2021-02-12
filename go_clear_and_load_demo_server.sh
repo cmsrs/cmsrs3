b0f405c6d6f80d6ac2494878859767284426b57a
cmsrs <robert.szczepanski@cmsrs.pl>
2021-02-12 11:50:12 +0100

    demo load script fix

--- go_clear_and_load_demo_server.sh
+++ go_clear_and_load_demo_server.sh
@@ -2,7 +2,7 @@ rm -rf public/images/page
 rm -rf public/images/product
 #./go_privilege.sh 
 
-php74 artisan migrate:refresh
+php74 artisan migrate:fresh
 php74 artisan db:seed
 php74 artisan command:load-demo-data
 
