9499b87e17777aad9226a790cb1a6d183dbe0d32
Robert <robert.szczepanski@cmsrs.pl>
2023-09-04 17:04:21 +0200

    add db pass in sh scripts

--- go_create_test_db.sh
+++ go_create_test_db.sh
@@ -1,2 +1,2 @@
-sudo mysql --default-character-set=utf8 -e  "DROP DATABASE IF EXISTS cmsrs_testing;"
-sudo mysql --default-character-set=utf8 -e  "CREATE DATABASE cmsrs_testing CHARACTER SET utf8 COLLATE utf8_general_ci;"
+sudo mysql --default-character-set=utf8 -u root -phaslo123  -e  "DROP DATABASE IF EXISTS cmsrs_testing;"
+sudo mysql --default-character-set=utf8 -u root -phaslo123  -e  "CREATE DATABASE cmsrs_testing CHARACTER SET utf8 COLLATE utf8_general_ci;"
