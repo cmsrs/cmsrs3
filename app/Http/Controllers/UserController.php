9f8c2e0862f1f81a8ad331e78c7b1d0737f58762
Robert <robert.szczepanski@cmsrs.pl>
2024-04-05 14:26:49 +0200

    search in created_at too

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -55,7 +55,9 @@ class UserController extends Controller
             ->when($search, function($query) use ($search) {
                 return $query->where(function($query) use ($search) {
                     $query->where('name', 'like', $search)
-                        ->orWhere('email', 'like', $search);
+                        ->orWhere('email', 'like', $search)
+                        ->orWhere('created_at', 'like', $search)                        
+                        ;
                 });
             })
             // ->where(function($query) use ($search) {
