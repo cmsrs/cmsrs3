80fe4d1e9685cf8389b949149bddcae1f6af2d2c
Robert <robert.szczepanski@cmsrs.pl>
2024-09-24 17:29:12 +0200

    add content in gallery

--- resources/views/gallery.blade.php
+++ resources/views/gallery.blade.php
@@ -3,6 +3,8 @@
 
 <h1 class="mb-4 mt-3">{{$page->translatesByColumnAndLang( 'title', $lang )}}</h1>
 
+<div class="mt-2">{!! $page->translatesByColumnAndLang( 'content', $lang ) !!}</div>
+
 <div id="gallery">
 </div>
 
