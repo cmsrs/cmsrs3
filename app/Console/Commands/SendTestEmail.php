4c1a3fd1c39d07d4c0168e1f44e36ff58389c703
Robert <robert.szczepanski@cmsrs.pl>
2024-11-10 18:14:49 +0100

    test email send

---
+++ app/Console/Commands/SendTestEmail.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Mail;
+
+class SendTestEmail extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:send-test-email {email}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'send test email';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $toEmail = $this->argument('email');
+
+        if (! $toEmail) {
+            echo 'put email receiver';
+            exit;
+        }
+
+        Mail::raw('this is test msg body', function ($message) use ($toEmail) {
+            $message->to($toEmail)
+                ->subject('this is test title');
+        });
+
+        // $toName = 'J K';
+        // $data = array('name'=>"Jan Kowalski", "body" => "Test mail");
+        // Mail::send('emails', $data, function($message) use ($toName, $toEmail) {
+        //     $message->to($toEmail, $toName)->subject('Artisans Web Testing Mail');
+        //     $message->from('test@gmail.com','Artisans Web');
+        // });
+    }
+}
