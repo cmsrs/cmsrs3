6acbe66332643ad434cc6c29cd576e1c437e1291
Robert <robert.szczepanski@cmsrs.pl>
2024-12-13 20:40:46 +0100

    remove contact from footer

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -3,9 +3,6 @@
     <span class="pull-left">
         <a style="color:grey;"  href="{{ $footerPages['policyUrl'] }}">{{ $footerPages['policyTitle'] }}</a>
     </span>
-    <span class="pull-right">
-        <a style="color:grey;"  href="{{ $footerPages['contactUrl'] }}">{{ $footerPages['contactTitle'] }}</a>
-    </span>
 </div>
 
 <script type="application/javascript" src="/js/lib/bootstrap.bundle.min.js"></script>
