2a6540bbe3f84cc51a5f05539f636c83222b39c0
Robert <robert.szczepanski@cmsrs.pl>
2025-07-15 19:59:56 +0200

    favicon

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -1,6 +1,11 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-<link rel="icon" href="../../../../favicon.ico">
+
+<link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+<link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+<link rel="shortcut icon" href="/favicon.ico" />
+<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+<link rel="manifest" href="/site.webmanifest" />
 
 <title>{{ $page_title ?? '' }}</title>
 <meta name="author" content="Robert Szczepanski - cmsRS">
