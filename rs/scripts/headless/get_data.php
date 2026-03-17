02afbbb1bed07e718068c789c859d5063a75c7c0
Robert <robert.szczepanski@cmsrs.pl>
2026-03-17 20:43:28 +0100

    headless script

--- rs/scripts/headless/get_data.php
+++ rs/scripts/headless/get_data.php
@@ -1,6 +1,6 @@
 <?php
 
-$domain = 'http://demo-headless.cmsrs.pl';
+$domain = 'http://demo.cmsrs.pl';
 
 // get default_lang
 $config = "curl -s  -H 'Accept:application/json' -H 'Content-Type:application/json' $domain/api/headless/config";
@@ -31,6 +31,23 @@ if ($res2->success) {
 } else {
     exit('something wrong with menus');
 }
+
+#main_page content
+$main_page_content = "curl -s -H 'Accept:application/json' -H 'Content-Type:application/json' $domain/api/headless/pages-type/main_page";
+echo $main_page_content;
+$out = [];
+exec($main_page_content, $out);
+$res34 = json_decode($out[0]);
+if ($res34->success) {
+    $main_page_content_data = $res34->data;
+} else {
+    exit('something wrong with main page datai 0');
+}
+
+print_r($main_page_content_data );
+//exit;
+
+
 
 // get data to main page
 $main_page = "curl -s -H 'Accept:application/json' -H 'Content-Type:application/json' $domain/api/headless/pages-type/inner";
