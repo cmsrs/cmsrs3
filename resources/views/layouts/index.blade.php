3ba4909be218d3dcf40177888b70ad2502c97017
cmsrs <robert.szczepanski@cmsrs.pl>
2026-03-22 19:26:48 +0100

    index.blade.php remove slider

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -4,7 +4,7 @@
    @include('includes.head')
    @includeIf('includes.mysite')   
    <script>
-      var slider_images = <?php echo json_encode( !empty($slider_images) ? $slider_images->toArray() : []) ?>;
+      var slider_images = [] <?php //echo json_encode( !empty($slider_images) ? $slider_images->toArray() : []) ?>;
    </script>
 
 </head>
@@ -19,11 +19,11 @@
 
       <?php
 
-         $box1 = $pageService->getPageDataByShortTitleCache( 'main_page_box1', 'content', $lang );
-         $box2 = $pageService->getPageDataByShortTitleCache( 'main_page_box2', 'content', $lang );
-         $box3 = $pageService->getPageDataByShortTitleCache( 'main_page_box3', 'content', $lang );
+         $box1 = ''; //$pageService->getPageDataByShortTitleCache( 'main_page_box1', 'content', $lang );
+         $box2 = ''; //$pageService->getPageDataByShortTitleCache( 'main_page_box2', 'content', $lang );
+         $box3 = ''; //$pageService->getPageDataByShortTitleCache( 'main_page_box3', 'content', $lang );
       ?>
-      <?php  if( $box1 && $box2 && $box3 ){ ?>
+      <?php  if( false && $box1 && $box2 && $box3 ){ ?>
       <div class="mt-5 container">
          <div class='row'>
               <div class='col-md-4'>
