afe95b3dc63e91fb848345ac70c31f025fd0cd75
Robert <robert.szczepanski@cmsrs.pl>
2026-05-25 17:58:39 +0200

    remove config from phpstan

--- app/Enums/Cmsrs/SortDirection.php
+++ app/Enums/Cmsrs/SortDirection.php
@@ -7,6 +7,9 @@ enum SortDirection: string
     case ASC = 'asc';
     case DESC = 'desc';
 
+    /**
+     * @return list<string>
+     */
     public static function values(): array
     {
         return array_column(self::cases(), 'value');
